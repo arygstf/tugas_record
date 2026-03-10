35f038248d52b169e3186159a6cd1aff4d43193c
arygstf <arygustafmuzaky@gmail.com>
2026-03-10 18:30:28 +0700

    start

---
+++ arr_mhs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Mahasiswa{
+    string npm, nama, jurusan;
+    int umur;
+    string hobi[3];
+}
+
+int main() {
+
+    
+    
+    
+    
+    
+    
+    return 0;
+}
